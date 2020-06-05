337137f8d6ff3b7e062f90821127ad25beb44e8e
Hadrian Hughes <hadrian.hughes@and.digital>
2020-06-05 10:56:23 +0100

    Use dist directory instead of build and include in git repo

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
     ]
   },
   output: {
-    path: path.resolve(__dirname, 'build'),
+    path: path.resolve(__dirname, 'dist'),
     filename: 'index.js',
     libraryTarget: 'commonjs2'
   },
